1ee3ce741cb462a8c8ea2c883eb5ed0c20358809
Jonghee Lee <79948405+K-107@users.noreply.github.com>
2021-10-25 16:31:05 +0900

    에폭 입력 타입 수정

--- train.py
+++ train.py
@@ -470,7 +470,7 @@ def get_args():
     parser.add_argument('--phase', choices=['train','test'], default='train')
     parser.add_argument('--dataset_path', default=r'/home/changwoo/hdd/datasets/mvtec_anomaly_detection2') # 'D:\Dataset\mvtec_anomaly_detection')#
     parser.add_argument('--category', default='bottle')
-    parser.add_argument('--num_epochs', default=1)
+    parser.add_argument('--num_epochs', type=int, default=1)
     parser.add_argument('--batch_size', default=1)
     parser.add_argument('--load_size', default=256) # 256
     parser.add_argument('--input_size', default=224)
